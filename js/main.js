6aa9db41d65f732ffc1647e0c5f8f0d9815db6ed
Imoa1991 <ordonez.im@gmail.com>
2018-07-12 10:51:34 +0200

    intento localStorage

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 var search = document.querySelector('.series');
 var buttonSearch = document.querySelector('.button');
 var ul = document.querySelector('ul');
+var saveFavorites = {
+  'div':'',
+};
 buttonSearch.addEventListener('click', see);
 search.addEventListener('keypress', enter);
 function enter(event) {
